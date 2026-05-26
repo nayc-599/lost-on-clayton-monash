f55e795b46155d3a9d06cdb2fc1524ffe75a7ba2
ame-lie-h <ameliehaigh@icloud.com>
2026-05-26 21:55:19 +1000

    Add files via upload

---
+++ loc_locations.py
@@ -0,0 +1,15 @@
+locations = [
+    {"name": "Alan Finkel Building", "lat": -37.90998, "lon": 145.13511},
+    {"name": "Biomedical Learning and Teaching Building", "lat": -37.912771, "lon": 145.129602},
+    {"name": "Campus Centre", "lat": -37.911801, "lon": 145.132970},
+    {"name": "Hargrave Andrew Library", "lat": -37.909682, "lon": 145.132065},
+    {"name": "Learning and Teaching Building", "lat": -37.9138247, "lon": 145.1325395},
+    {"name": "Louis Matheson Library", "lat": -37.912811279296875, "lon": 145.13414001464844},
+    {"name": "Maths Building", "lat": -37.9091946, "lon": 145.1308962},
+    {"name": "Robert Menzies Building", "lat": -37.9125934, "lon": 145.1326706},
+    {"name": "Monash Sport", "lat": -37.9128915, "lon": 145.1364703},
+    {"name": "Science Lectures", "lat": -37.9104879, "lon": 145.1308102},
+    {"name": "Senior Physics", "lat": -37.9102279, "lon": 145.1308254},
+
+
+]
