f7d7c5fe737622cce2c8c073f481eb42ba0a2ead
vrunda-mistry <151717287+vrunda-mistry@users.noreply.github.com>
2026-05-28 16:41:55 +1000

    allocated buildings to faculties

--- subjects per building/scraper/buildings_faculties.py
+++ subjects per building/scraper/buildings_faculties.py
@@ -3,3 +3,22 @@
 from __future__ import annotations
 
 # we have 11 buildings, map faculties that host their classes in the buildings to the building
+FACULTY_TO_BUILDINGS: dict[str, list[str]] = {
+    "Arts": ["Menzies", "Matheson Library", "LTB", "Monash Sport"],
+    
+    "Arts, Design and Architecture": ["Menzies", "Matheson Library", "LTB"],
+    
+    "Business and Economics": ["BLTB", "Campus Centre", "LTB", "Matheson Library", "Mathematics Building", "Menzies", "Monash Sport", "Science Lectures"],
+    
+    "Engineering": ["Alan Finkle", "Hargrave Library", "LTB", "Mathematics Building"],
+    
+    "Information Technology": ["Alan Finkle", "Hargrave Library", "LTB"],
+    
+    "Science": ["BLTB", "Hargrave Library", "LTB", "Mathematics Building", "Science Lectures", "Senior physics"],
+    
+    "Education": ["Matheson Library", "LTB", "Monash Sport", "Science Lectures"],
+    
+    "Law": ["Menzies", "LTB"],
+    
+    "Medicine, Nursing and Health Sciences": ["BLTB", "Hargrave Library", "Monash Sport", "Science Lectures"]
+} 
